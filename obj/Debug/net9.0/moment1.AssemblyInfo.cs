80ae533adb2880e28a38df3ba161047dfedb52fd
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-27 23:58:10 +0100

    fixat allt content till second

--- obj/Debug/net9.0/moment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c3a2118bd27166f0524268364551d7cb80785c")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
