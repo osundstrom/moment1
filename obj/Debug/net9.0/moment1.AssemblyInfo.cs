0e39c160391b2ec662e40f50ef82c4abffbefc7c
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-29 15:13:02 +0100

    ändrat felskrivning

--- obj/Debug/net9.0/moment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b04b410a03f4305123755b9a60c7613491739ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4250a9739972dc11f933cc7ef83c7db3a4c0d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
