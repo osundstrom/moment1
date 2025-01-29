8a4250a9739972dc11f933cc7ef83c7db3a4c0d7
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-29 11:14:13 +0100

    lagt kommentarer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,12 +1,16 @@
 ﻿
-
+//eventlistener vid laddad sida (DOM)
 document.addEventListener("DOMContentLoaded", function () {
+    //väljer element
     const selectedElem = document.getElementById("dateToday");
+
+    //nytt datum
     const newDate = new Date();
 
-   
+   //sätter till localt datum
     const fullDate = newDate.toLocaleDateString("sv-SE");
     console.log(fullDate)
    
+    //sätter elemnt till fullDate
     selectedElem.textContent = fullDate;
 });
